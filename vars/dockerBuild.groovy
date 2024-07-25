f5e1b38eb63605afa62e72bb6e35b36072149558
Raju Nandi <nandir@mskcc.org>
2024-07-25 17:50:29 +0530

    updated jenkinsfile

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,5 +1,5 @@
 def call(String imageName, String buildArgs = "") {
-    docker.withRegistry(docker.io, devops091/rajunandi91!) {
+    docker.withRegistry("docker.io", "devops091/rajunandi91!") {
         def customImage = docker.build("${imageName}:${env.BUILD_ID}", buildArgs + " .")
         customImage.push()
     }
