0d35f4b436639a214ede138070ca8298c478e1c1
Raju Nandi <nandir@mskcc.org>
2024-07-25 17:10:28 +0530

    removed extra files

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,4 +1,4 @@
 def call(String imageName, String buildArgs="") {
-def customImage = docker.build("${imageName}:${env.BUILD_ID}", "${buildArgs} .)
+def customImage = docker.build("${imageName}:${env.BUILD_ID}", "${buildArgs} .")
     customImage.push()
 }
