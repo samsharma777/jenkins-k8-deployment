af7eca5df9e2b09cb641599e14f4831e8d92486e
Raju Nandi <nandir@mskcc.org>
2024-07-25 17:40:39 +0530

    updated jenkinsfile

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,5 +1,5 @@
 def call(String imageName, String buildArgs = "", String registry, String credentialsId) {
-    docker.withRegistry(registry, credentialsId) {
+    docker.withRegistry("https://${registry}", credentialsId) {
         def customImage = docker.build("${imageName}:${env.BUILD_ID}", buildArgs + " .")
         customImage.push()
     }
