8deaf55d086fbfbf25baa06fda67c931cb7b6c12
Raju Nandi <nanditechbytes@gmail.com>
2024-07-30 10:25:09 +0530

    Update kubeSecret.groovy

--- vars/kubeSecret.groovy
+++ vars/kubeSecret.groovy
@@ -1,14 +1,14 @@
 def call(String secret_type, String secret_name, String cert_password) {
-// Check if the secret already exists
-    def secretExists = sh(script: 'kubectl get secret $secret_name --ignore-not-found', returnStatus: true) == 0
-                
-        if (!secretExists) {
-                    // If the secret does not exist, create it
-         sh """
-            kubectl create secret $secret_type $secret_name --from-literal=password=$cert_password
-            """
-            echo "Secret $secret_name created."
-            } else {
-            echo "Secret $secret_name already exists. Skipping creation."
+    // Check if the secret already exists
+    def secretExists = sh(script: "kubectl get secret ${secret_name} --ignore-not-found", returnStatus: true) == 0
+
+    if (!secretExists) {
+        // If the secret does not exist, create it
+        sh """
+            kubectl create secret ${secret_type} ${secret_name} --from-literal=password=${cert_password}
+        """
+        echo "Secret ${secret_name} created."
+    } else {
+        echo "Secret ${secret_name} already exists. Skipping creation."
     }
 }
